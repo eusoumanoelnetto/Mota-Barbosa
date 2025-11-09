cf3c0a0bc80b35b61ac65ca7c6c3c4465c28be8c
eusoumanoelnetto <manoelccoelho@gmail.com>
2025-11-09 18:32:43 -0300

    revert: restore index.html and index.tsx; remove test files

--- index.tsx
+++ index.tsx
@@ -6,7 +6,6 @@ import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  document.body.innerHTML = '<div style="padding: 20px; color: red;">Error: Root element not found</div>';
   throw new Error("Could not find root element to mount to");
 }
 
